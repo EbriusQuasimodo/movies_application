351b89a757a4308df32ffe599316684c66c28738
EbriusQuasimodo <sdohnisukaloveu@gmail.com>
2022-08-26 17:45:46 +0500

    Add dio

--- lib/resources/movie_repository/movie_repository.dart
+++ lib/resources/movie_repository/movie_repository.dart
@@ -1,9 +1,8 @@
 import 'package:movies_app/models/movie_model.dart';
-import 'package:movies_app/resources/api_client/api_client.dart';
 import 'package:movies_app/resources/api_client/api_client_dio.dart';
 
 class MovieRepository {
-  ApiClientDio _apiClient = ApiClientDio();
+  ApiClientDio apiClient = ApiClientDio();
 
-  Future<List<Docs>> fetchAllMovies() => _apiClient.getMovieDio();
+  Future<List<Docs>> fetchAllMovies() => apiClient.getMovieDio();
 }
